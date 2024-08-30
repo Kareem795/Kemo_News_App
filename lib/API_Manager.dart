876efed40e06c558c35bade7e980714744acf412
Kareem795 <kemoyasser2003@gmail.com>
2024-08-30 21:01:26 +0300

    There is a problem in the news ui folder.

---
+++ lib/API_Manager.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:kemo_news_app/constants.dart';
+import 'package:kemo_news_app/data/model/News_Data_Response.dart';
+import 'package:kemo_news_app/data/model/Sources_Responces.dart';
+
+class ApiManager {
+  static Future<SourcesResponse> getSources() async {
+    Uri url = Uri.https(
+        Constants.BaseURL, "/v2/everything", {"api_key": Constants.API_Key});
+
+    http.Response response = await http.get(url);
+
+    var json = jsonDecode(response.body);
+
+    SourcesResponse sources_responces = SourcesResponse.fromJson(json);
+
+    return sources_responces;
+  }
+
+  static Future<News_Data_Response> getNewsData(String sourceID) async {
+    Uri url = Uri.https(Constants.BaseURL, "/v2/everything", {
+      "sources": sourceID,
+    });
+
+    http.Response response = await http.get(
+      url,
+      headers: {
+        "api_key": Constants.API_Key,
+      },
+    );
+
+    var json = jsonDecode(response.body);
+
+    News_Data_Response model = News_Data_Response.fromJson(json);
+
+    return model;
+  }
+}
---
+++ lib/data/model/news_ui.dart
@@ -0,0 +1,96 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:kemo_news_app/API_Manager.dart';
+import 'package:kemo_news_app/data/model/Articles_Response.dart';
+import 'package:kemo_news_app/data/model/Sources_Responces.dart';
+import 'package:kemo_news_app/tabs_items.dart';
+
+class News_UI extends StatefulWidget {
+  const News_UI({super.key});
+
+  @override
+  State<News_UI> createState() => _News_UIState();
+}
+
+class _News_UIState extends State<News_UI> {
+  int selected_tap_index = 0;
+  List<Sources> sources = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        FutureBuilder(
+          future: ApiManager.getSources(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            if (snapshot.hasError) {
+              return Center(child: Text("Something went wrong"));
+            }
+
+            sources = snapshot.data?.sources ?? []; 
+
+            // return ListView.builder
+            // (
+            //   itemBuilder: (context, index)
+            //   {
+            //     return ListTile
+            //      (
+            //       title: Text(sources[index].name ?? ""),
+            //     );
+            //   },
+            //   itemCount: sources.length,
+            // );
+
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: DefaultTabController(
+                  length: sources.length,
+                  child: TabBar(
+                    isScrollable: true,
+                    indicatorColor: Colors.transparent,
+                    dividerColor: Colors.transparent,
+                    onTap: (value) {
+                      selected_tap_index = value;
+                      setState(() {});
+                    },
+                    tabs: sources
+                        .map((e) => Tabs_Items(
+                              source: e,
+                              is_selected:
+                                  sources.elementAt(selected_tap_index) == e,
+                            ))
+                        .toList(),
+                  )),
+            );
+          },
+        ),
+        FutureBuilder(
+          future: ApiManager.getNewsData(sources[selected_tap_index].id),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            if (snapshot.hasError) {
+              return Center(child: Text("Something went wrong"));
+            }
+
+            var articles = snapshot.data?.articles ?? [];
+
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                return Text(articles[index].title ?? "");
+              },
+              itemCount: articles.length,
+            );
+          },
+        )
+      ],
+    );
+  }
+}
